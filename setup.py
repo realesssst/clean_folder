cf80c7d763d14937369433b5bb8b3c22b45c9556
realesssst <139008025+realesssst@users.noreply.github.com>
2023-07-28 20:06:18 +0300

    py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ setup(name='clean_folder',
       version='0.0.1',
       description='clean_folder',
       packages=find_namespace_packages(),
-      entry_points={'console_scripts': ['clean-folder=clean_folder.main:start']}
+      entry_points={'console_scripts': ['clean-folder=clean_folder.clean:start']}
       )     
